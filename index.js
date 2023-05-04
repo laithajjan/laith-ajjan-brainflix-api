e3ca78f9241b5011e470672bf1c479e0e15d78a4
laithajjan <laithajjan@gmail.com>
2023-05-03 19:42:42 -0600

    Few changes to index.js for optimization

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-// Required modules
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const videoRoutes = require("./routes/videos");
 
-// Configuration
 dotenv.config();
 const PORT = process.env.PORT || 8080;
 
-// Initialize app
 const app = express();
 
-// Middleware setup
 const configureMiddleware = () => {
   app.use(cors());
   app.use(express.json());
@@ -19,14 +15,12 @@ const configureMiddleware = () => {
   app.use("/", videoRoutes);
 };
 
-// Start server
 const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
   });
 };
 
-// Main function to setup and start the server
 const main = () => {
   configureMiddleware();
   startServer();
